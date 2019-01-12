28ed469292d49aec7d21951128460b2531907116
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-12 17:15:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,12 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 client.on('message', message => {
-    console.log(client.message.content);
+    console.log(client.user.message.content);
+    if (message.content === `${prefix}ping`) {
+    message.channel.send('Pong.');
+}
+else if (message.content === `${prefix}beep`) {
+    message.channel.send('Boop.');
+}
 });
 client.login(token);
