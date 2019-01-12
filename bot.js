4284e3d2a433b831b7489753c09b30e2c9456f24
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-12 16:40:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = process.env.PREFIX;
+const token = process.env.TOKEN;
+const ownerid = process.env.OWNER_ID;
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login('token');
+client.login(token);
