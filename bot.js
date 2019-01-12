c6aee781200c9a43b54875321742bf7b5cac096a
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-12 16:38:09 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('NTMzNjUxNjI1MjgyMzcxNTk0.DxuLCQ.GDEt3cqbnVSr8xN6lJV7uIYfypQ');
